382f2f21ddc4b2fe425621d10d77e10ed62e4628
Dima Andreyuk <dima.andreyuk@teacode.io>
2023-09-20 23:18:39 +0200

    4.0.0a2 fixes 2

--- ios/RCTVLCPlayer/RCTVLCPlayer.m
+++ ios/RCTVLCPlayer/RCTVLCPlayer.m
@@ -363,8 +363,17 @@ static NSString *const playbackRate = @"rate";
 
 
 -(void)setVideoAspectRatio:(NSString *)ratio{
-    char *char_content = [ratio cStringUsingEncoding:NSASCIIStringEncoding];
-    [_player setVideoAspectRatio:char_content];
+    // Convert the C-style string to an NSString
+    NSString *aspectRatioString = [NSString stringWithCString:ratio encoding:NSASCIIStringEncoding];
+
+    // Check if the conversion was successful
+    if (aspectRatioString) {
+        // Pass the NSString to the _player's method
+        [_player setVideoAspectRatio:aspectRatioString];
+    } else {
+        // Handle the case where the conversion failed (e.g., invalid encoding)
+        NSLog(@"Failed to convert C-string to NSString.");
+    }
 }
 
 -(void)setRecordingStateStream:(NSString *)recordingStateStream{
